409f9f506730898830c182e29b3f3a3dce5b91a3
Amna ali <amnaalii7115@gmail.com>
2026-08-27 13:39:45 +0500

    Update website

--- src/features/banking/index.ts
+++ src/features/banking/index.ts
@@ -1,15 +1,3 @@
 export * from "./components";
 export * from "./schemas";
 export * from "./types";
-export type {
-  BankRegisterEntry,
-  BankTransaction,
-  CreateTransactionPayload,
-  TransactionDirection,
-  TransactionFilters,
-  TransactionLine,
-  TransactionLineType,
-  TransactionStatus,
-  TransactionType,
-  VoidTransactionPayload,
-} from "./transaction";
